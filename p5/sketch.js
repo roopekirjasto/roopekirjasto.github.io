d91a1c544748c0bd4cd32b1be745c9bdf4226ca2
Roope Garcia <roopedev@gmail.com>
2019-01-01 03:29:51 +0000

    Finna API

---
+++ p5/sketch.js
@@ -0,0 +1,38 @@
+var api = 'https://api.finna.fi/api/v1/search?lookfor=';
+var city = '&type=AllFields&field[]=rawData&sort=relevance%2Cid%20asc&page=1&limit=20&prettyPrint=false&lng=fi';
+
+function setup() {
+	noCanvas();
+  var button = select('#submit');
+  button.mousePressed(weatherAsk);
+  
+  input = select('#hae');
+  
+}
+
+  function weatherAsk() {
+    var url = api + input.value() + city;
+    loadJSON(url, gotData);
+
+}
+
+function gotData(data) {
+
+  var articles = data.records;
+    
+  for (var i = 0; i < articles.length; i++) {
+    createElement('h2', articles[i].rawData.author[0]).addClass('author');
+      for (var j = 0; j < articles.length; j++) {
+      	createElement('h5', 'ISBN: ' + articles[j].rawData.isbn[0]).addClass('btn btn-lg btn-primary');
+        	for (var e = 0; e < articles.length; e++) {
+      			createImg(articles[e].rawData.url[0]).addClass('');
+            	for (var b = 0; b < articles.length; b++) {
+                createElement('p', 'Hyllypaikka: ' + articles[b].rawData.classification_str_mv[1]).addClass('btn btn-lg btn-success');
+                	for (var y = 0; y < articles.length; y++) {
+                		createA('', articles[y].rawData.title[1])('btn btn-primary');
+        	}
+        }
+			}
+		}
+	}
+}
