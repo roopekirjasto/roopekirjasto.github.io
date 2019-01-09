011f75794fa10c11632d6d00a902adeada8053e2
Roope Garcia <roopedev@gmail.com>
2019-01-09 12:39:50 +0000

    finna search v2

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,6 +1,8 @@
 var api = 'https://api.finna.fi/api/v1/search?lookfor=';
 var city = '&type=AllFields&field[]=rawData&sort=relevance%2Cid%20asc&page=1&limit=20&prettyPrint=false&lng=fi';
 
+
+
 function setup() {
 	noCanvas();
   var button = select('#submit');
@@ -27,9 +29,9 @@ function gotData(data) {
         	for (var e = 0; e < articles.length; e++) {
       			createImg(articles[e].rawData.url[0]).addClass('');
             	for (var b = 0; b < articles.length; b++) {
-                createElement('p', 'Hyllypaikka: ' + articles[b].rawData.classification_str_mv[1]).addClass('btn btn-lg btn-success');
+                createElement('li', 'Hyllypaikka: ' + articles[b].rawData.classification_str_mv[1]).addClass('btn btn-lg btn-success');
                 	for (var y = 0; y < articles.length; y++) {
-                		createA('', articles[y].rawData.url[0])('btn btn-lg btn-primary');
+                		createElement('p', articles[y].rawData.physical[0])('btn btn-lg btn-primary');
         	}
         }
 			}
